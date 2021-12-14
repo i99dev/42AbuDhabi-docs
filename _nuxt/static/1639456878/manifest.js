552fd42978117b6c4892b6c7bb6d1701f2493f08
i99dev <i99dev@users.noreply.github.com>
2021-12-14 04:42:11 +0000

    deploy: e0aa92529a209f33f83c1374d7687cda67f039fd

--- _nuxt/static/1639456878/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Far\u002Freleases","\u002Freleases","\u002Far","\u002Far\u002Fprojects\u002Flibft","\u002Far\u002Fguidance\u002F42-ValgrindContainer","\u002Fprojects\u002Flibft","\u002Fguidance\u002F42-ValgrindContainer"]})
